bb3a69154b521aa806c82a291aa877e6994d36a9
wongpakho814 <wongpakho814@gmail.com>
2022-10-10 18:32:16 +1100

    included packages and filled in the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your project's title?",
+    "Description of the project (e.g. What was your motivation? What problem does it solve?): ",
+    "What are the steps required to install your project?",
+    "The instructions and examples for use: ",
+    "Please list the collaborators here: ",
+    "Please provide examples on how to run the tests, if any: ",
+    "Please choose the license used in this project, if any: "
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: questions[0],
+        },
+        {
+            type: "input",
+            name: "description",
+            message: questions[0],
+        },
+    ])
+
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
