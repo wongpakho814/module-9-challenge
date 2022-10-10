4e8f0dbf8d989fb0b3a962845257df581b5c4eb7
wongpakho814 <wongpakho814@gmail.com>
2022-10-10 18:55:42 +1100

    Finished the inquirer related codes

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const fileName = "README.md";
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -24,12 +25,63 @@ inquirer
         {
             type: "input",
             name: "description",
-            message: questions[0],
+            message: questions[1],
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: questions[2],
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: questions[3],
+        },
+        {
+            type: "input",
+            name: "credits",
+            message: questions[4],
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: questions[5],
+        },
+        {
+            type: "list",
+            name: "license",
+            message: questions[6],
+            choices: [
+                "apache-2.0", 
+                "bsl-1.0",
+                "bsd-3-clause", 
+                "bsd-2-clause", 
+                "cc0-1.0", 
+                "cc-by-4.0", 
+                "cc-by-sa-4.0",
+                "epl-1.0",
+                "gpl-3.0",
+                "gpl-2.0",
+                "agpl-3.0",
+                "gpl-3.0",
+                "isc",
+                "mit",
+                "mpl-2.0",
+                "ofl-1.1",
+                "unlicense",
+                "zlib"
+            ],
         },
     ])
-
+    .then((data) => {
+        writeToFile(fileName, data);
+    });
+    
 function writeToFile(fileName, data) {
-
+    const READMEContent = generateMarkdown(data);
+    fs.writeFile(fileName, READMEContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    );
 }
 
 // TODO: Create a function to initialize app
